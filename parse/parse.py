df90812329358eecd67b42c37061e17cb79f93dd
minglangdu <mldu@cydu.net>
2025-09-17 18:39:22 -0700

    Add reading files to JSON

--- parse/parse.py
+++ parse/parse.py
@@ -1,5 +1,6 @@
 import requests
 import pdfplumber
+import json
 
 URL = "https://science.osti.gov/-/media/wdts/nsb/pdf/HS-Sample-Questions/Sample-Set-1/round1.pdf"
 
@@ -12,6 +13,8 @@ HEADERS = {
 
 FILE = "packet.pdf"
 
+RESULT = "packet.json"
+
 response = requests.get(URL, headers=HEADERS)
 if response.status_code == 200:
     with open(FILE, 'wb') as f:
@@ -19,4 +22,44 @@ if response.status_code == 200:
     print("Wrote packet.")
 else:
     print("Request blocked.")
+    raise Exception("Request Blocked")
 
+
+content = ""
+with pdfplumber.open(FILE) as pdf:
+    for page in pdf.pages:
+        text = page.extract_text()
+        if text:
+            content += text + "\n"
+
+data = []
+
+next = {}
+inmult = False
+for line in content.splitlines():
+    if ("ANSWER: " in line):
+        after = line.rsplit("ANSWER: ", 1)[1]
+        next["answer"] = after
+        data.append(next)
+        next = {}
+        inmult = False
+    if (inmult):
+        next["content"] += "\n" + line
+    if ("TOSS-UP" in line):
+        next["type"] = "Toss-up"
+    if ("BONUS" in line):
+        next["type"] = "Bonus"
+    if ("Short Answer" in line):
+        next["answertype"] = "Short Answer"
+        after = line.rsplit("Short Answer", 1)[1]
+        next["content"] = after
+    if ("Multiple Choice" in line):
+        inmult = True
+        next["answertype"] = "Multiple Choice"
+        after = line.rsplit("Multiple Choice", 1)[1]
+        next["content"] = after
+    
+    
+
+with open(RESULT, "w") as file:
+    json.dump(data, file, indent=4)
